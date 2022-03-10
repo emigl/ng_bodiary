7fb16ac0d41803af64f8eea4b5429c65740db333
Emi Gomez <emilio98gl@gmail.com>
2022-03-10 19:36:55 +0100

    agregado angular material y creada la estructura inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/index/login/login.component';
+import { RegisterComponent } from './components/index/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Cambiar cuando haya una página de welcome.
+  {path: '', redirectTo: '/login', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  // Cambiar a una página de error cuando esté completada.
+  {path: '*', component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+// Modules
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
+// Components
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/index/login/login.component';
+import { RegisterComponent } from './components/index/register/register.component';
+
+// Material
+import { MatSliderModule } from '@angular/material/slider';
+import { LoginAdminComponent } from './components/admin/login-admin/login-admin.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LoginAdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
