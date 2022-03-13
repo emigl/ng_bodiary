f144928b181cff186f5f4ad7aa08dfa9e5111a98
Emi Gomez <emilio98gl@gmail.com>
2022-03-13 19:05:38 +0100

    servicio completado, falta probar

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,47 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  appUrl: string;
+  apiUrl: string;
+
+  constructor(private http: HttpClient) { 
+    this.appUrl = environment.appUrl;
+    this.apiUrl = 'Login';
+
+  }
+
+   // Post a /api/Login que sirve para comprobar si existe un user en la bdd y poder logearse.
+   login(user: User): Observable<any> {
+
+    // Como primer parámetro coge la URL a donde apunta, como segundo parámetro, la información que se va a pasar por ahí.
+    return this.http.post(this.appUrl+this.apiUrl,user);
+  }
+
+  setLocalStorage(data: string): void {
+    localStorage.setItem('token', data);
+  }
+  
+  // getTokenDecoded(): string {
+  //   const helper = new JwtHelperService();
+
+  //   const decodedToken = helper.decodeToken(localStorage.getItem('token') || "Cannot get token" );
+
+  //   return decodedToken;
+  // }
+
+  rmLocalStorage(): void{
+    localStorage.removeItem('token');
+  }
+
+  getToken():string | null {
+
+    return localStorage.getItem('token') || null;
+  }
 }
