d3c4bb9ad00421977b34403297691e80e4de59d6
Emi Gomez <emilio98gl@gmail.com>
2022-03-14 18:53:40 +0100

    Login completado

--- src/app/components/index/login/login.component.ts
+++ src/app/components/index/login/login.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { User } from 'src/app/models/User';
 import { LoginService } from 'src/app/services/login.service';
 
 @Component({
@@ -14,11 +16,12 @@ export class LoginComponent implements OnInit {
   loading = false;
   constructor(private fb: FormBuilder,
               private router: Router,
-              private loginService: LoginService) {
+              private loginService: LoginService,
+              private snackBar: MatSnackBar) {
                 this.loginForm = this.fb.group({
-                  email: ['', [Validators.required, Validators.email]],
+                  email: ['admin@gmail.com', [Validators.required, Validators.email]],
                   // Mostrar el minimo de longitud en el html
-                  password: ['',[Validators.required, Validators.minLength(6)]],
+                  password: ['123456',[Validators.required, Validators.minLength(6)]],
                   // Si da error puede ser el valor false.
                   remember: [false, Validators.required]
 
@@ -29,33 +32,37 @@ export class LoginComponent implements OnInit {
 
   }
   getErrorMessage(){
-    
+    this.snackBar.open('Las credenciales no son válidas, ¡inténtalo de nuevo!', 'Cerrar', {
+      
+    })
   }
   login():void{
     this.loading = true;
     
     
-    //   const user: User = {
-    //     email: this.loginForm.value.email,
-    //     password: this.loginForm.value.password,
-    //   }
-    //   )
-    //   this.loginService.login(user).subscribe(data => {
+      const user: User = {
+        email: this.loginForm.value.email,
+        password: this.loginForm.value.password,
+      }
+      
+      this.loginService.login(user).subscribe(data => {
 
-    //     console.log(data);
-         // Save Jwt token to Local Storage
-    //     this.loginService.setLocalStorage(data.token);
+        console.log(data);
+        //  Save token to Local Storage
+        this.loginService.setLocalStorage(data.access_token);
 
-    //     this.toastr.success('Ya puedes ver los cuestionarios', 'Acceso concedido');
+        // this.toastr.success('Ya puedes ver los cuestionarios', 'Acceso concedido');
 
-    //     this.loading = false;
-    //     this.router.navigate(['/dashboard']);
-    //   }, err => {
-    //     console.log(err);
-    //     var { message } = err.error;
-    //     this.toastr.error(message, 'Inicio de sesión incorrectos');
-    //     this.loading = false;
-      // })
+        this.loading = false;
+        this.router.navigate(['/register']);
+      }, err => {
+        console.log(err);
+        var { message } = err.error;
+        // this.toastr.error(message, 'Inicio de sesión incorrectos');
+        console.log('message', message);
+        this.getErrorMessage();
+        this.loading = false;
+      })
 
 }
 
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
+import { User } from '../models/User';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class LoginService {
 
   constructor(private http: HttpClient) { 
     this.appUrl = environment.appUrl;
-    this.apiUrl = 'Login';
+    this.apiUrl = 'auth/login';
 
   }
 
@@ -21,20 +22,12 @@ export class LoginService {
    login(user: User): Observable<any> {
 
     // Como primer parámetro coge la URL a donde apunta, como segundo parámetro, la información que se va a pasar por ahí.
-    return this.http.post(this.appUrl+this.apiUrl,user);
+    return this.http.post(this.appUrl+this.apiUrl, user);
   }
 
   setLocalStorage(data: string): void {
     localStorage.setItem('token', data);
   }
-  
-  // getTokenDecoded(): string {
-  //   const helper = new JwtHelperService();
-
-  //   const decodedToken = helper.decodeToken(localStorage.getItem('token') || "Cannot get token" );
-
-  //   return decodedToken;
-  // }
 
   rmLocalStorage(): void{
     localStorage.removeItem('token');
